c05bd4ee6c0dccbae58282ac21276cd411cf64d8
Adam Steinert <adam.steinert@outlook.com>
2018-02-02 05:25:34 -0600

    Add install powershell script

---
+++ Install.ps1
@@ -0,0 +1,46 @@
+# Set-ExecutionPolicy Bypass -Scope Process -Force; .\Install.ps1
+
+# Utility
+#
+choco install notepadplusplus -y
+
+choco install 7zip.install -y
+
+choco install greenshot -y
+
+choco install vlc -y
+
+choco install paint.net -y
+
+choco install filezilla -y
+
+choco install foxitreader -y
+
+choco install dropbox -y
+
+choco install spotify -y
+
+choco install veracrypt -y
+
+# Communication
+#
+choco install slack -y
+
+choco install discord -y
+
+
+# Development
+#
+choco install git -y
+
+choco install gitkraken -y
+
+choco install nodejs.install -y
+
+# Will need to run setup and install optional components when done
+choco install visualstudiocode -y
+
+choco install visualstudio2017enterprise -y
+
+choco install dotnetcore-sdk -y
+
